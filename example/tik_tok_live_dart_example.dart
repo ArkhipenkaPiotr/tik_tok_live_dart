9ff91c9ee5bc97ac77f79f791951544d47e6a9b1
Piotr.Arkhipenka <arhipenkoqwerty@gmail.com>
2022-12-16 00:27:58 +0400

    init commit

---
+++ example/tik_tok_live_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:tik_tok_live_dart/tik_tok_live_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/tik_tok_live_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library tik_tok_live_dart;
+
+export 'src/tik_tok_live_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
