79c9ba512c5cf7b18572fc8fa093401a121c60dc
Piotr.Arkhipenka <arhipenkoqwerty@gmail.com>
2022-12-19 23:10:13 +0400

    Correct http client

--- lib/src/client/tik_tok_http_client.dart
+++ lib/src/client/tik_tok_http_client.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:tik_tok_live_dart/src/client/requests/response_with_cookies.dart';
+import 'package:tik_tok_live_dart/src/client/requests/tik_tok_cookie_jar.dart';
 import 'package:tik_tok_live_dart/src/client/tik_tok_request_settings.dart';
 import 'package:tik_tok_live_dart/src/errors/tik_tok_live_exception.dart';
 
@@ -7,6 +8,8 @@ import '../generated/proto/tiktok_schema.pb.dart';
 
 class TikTokHttpClient {
   static int _uuc = 0;
+  final _currentHeaders = <String, dynamic>{};
+  final _dio = Dio();
 
   TikTokHttpClient([int timeOut = 15000]) {
     _uuc++;
@@ -59,10 +62,23 @@ class TikTokHttpClient {
     Map<String, dynamic>? parameters,
     bool signUrl = false,
   }) async {
-    final request = _buildRequest(
-        signUrl ? await _getSignedUrl(url, parameters) : url, signUrl ? null : parameters);
-
-    return await request.get();
+    final String preparedUrl;
+    if (signUrl) {
+      preparedUrl = await _getSignedUrl(url, parameters);
+    } else {
+      preparedUrl = url;
+    }
+    final response = await _dio.get(preparedUrl,
+        options: Options(headers: _currentHeaders), queryParameters: parameters);
+    final cookies = response.headers['Set-Cookie'];
+    final cookieJar = TikTokCookieJar();
+    if (cookies != null) {
+      for (final cookie in cookies) {
+        final splitted = cookie.replaceAll(';', '').split('=');
+        cookieJar.set(splitted.first, splitted[1]);
+      }
+    }
+    return ResponseWithCookies(response.data, cookieJar);
   }
 
   Future<Map<String, dynamic>> _postRequest({
@@ -71,13 +87,15 @@ class TikTokHttpClient {
     Map<String, dynamic>? parameters,
     bool signUrl = false,
   }) async {
-    final request = _buildRequest(
-        signUrl ? await _getSignedUrl(url, parameters) : url, signUrl ? null : parameters);
-    return await request.post(data);
-  }
-
-  ITikTokHttpRequest _buildRequest(String url, [Map<String, dynamic>? parameters]) {
-    return TikTokHttpRequest(url).setQueries(parameters);
+    final String preparedUrl;
+    if (signUrl) {
+      preparedUrl = await _getSignedUrl(url, parameters);
+    } else {
+      preparedUrl = url;
+    }
+    final response = await _dio.post(preparedUrl,
+        options: Options(headers: _currentHeaders), queryParameters: parameters);
+    return response.data;
   }
 
   Future<String> _getSignedUrl(String url, [Map<String, dynamic>? parameters]) async {
@@ -88,23 +106,21 @@ class TikTokHttpClient {
       paramsString = parameters.keys.map((key) => '$key = ${parameters[key]}').join('&');
     }
     final parsedParameters = paramsString.isEmpty ? '' : '?$paramsString';
-    final request = TikTokHttpRequest(TikTokRequestSettings.tikTokSignApi);
-    request.setQueries({
+    final result = await _dio.get(TikTokRequestSettings.tikTokSignApi, queryParameters: {
       "client": "ttlive-net",
       "uuc": _uuc,
       "url": url + parsedParameters,
     });
-    final content = await request.get();
+    final body = result.data;
     try {
-      final signedUrl = content['..signedUrl'];
-      final userAgent = content['..User-Agent'];
-      TikTokHttpRequest.currentHeaders.remove("User-Agent");
-      TikTokHttpRequest.currentHeaders["User-Agent"] = userAgent;
+      final signedUrl = body['..signedUrl'];
+      final userAgent = body['..User-Agent'];
+      _currentHeaders["User-Agent"] = userAgent;
       return signedUrl;
     } catch (e) {
       throw TikTokLiveException(
-          "Insufficent values have been supplied for signing. Likely due to an update. Post an issue on GitHub.",
-          e);
+        "Insufficent values have been supplied for signing. Likely due to an update. Post an issue on GitHub.",
+      );
     }
   }
 }
